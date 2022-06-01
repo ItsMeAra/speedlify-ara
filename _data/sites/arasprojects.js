d3bb8c7f305dfa96c07d0d246b52566d58279a57
Ara L Abcarians <arala22@gmail.com>
2022-06-01 16:04:27 -0700

    Update and rename sample.js to arasprojects.js

---
+++ _data/sites/arasprojects.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: "Ara's Projects", // optional, falls back to object key
+	description: "My projects",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://www.itsmeara.com/",
+		"https://www.kandji.io/",
+		"https://www.firmwilloughby.com/"
+	]
+};
